a2851033a0f80e0c9f4b2518fbe9d92340a6f9b7
Kellen Malek <kellenmalek@hotmail.com>
2022-03-22 08:10:11 -0700

    Upgrade Astro and fix some styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: [],
+export default defineConfig({
+	integrations: [
+		sitemap(),
+		tailwind(),
+	],
 	buildOptions: {
 		site: "https://kjmalek.info",
 		sitemap: true,
