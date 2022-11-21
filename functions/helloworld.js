089ec6966d23376c02fcdd2a78eb32804ef00227
Kellen Malek <kellenmalek@hotmail.com>
2022-11-21 14:39:16 -0800

    Test out TinaCMS

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,13 @@
+const data = {
+  hello: "world",
+};
+
+const json = JSON.stringify(data, null, 2);
+
 export function onRequest(context) {
-  return new Response("Hello, world!");
+  return new Response(json, {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  });
 }
